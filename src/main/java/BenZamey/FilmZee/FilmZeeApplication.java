f1f7986530dfbc66e9c5890ba9fef68e920dfc83
Yahaya Ben Zamey <79756073+Benaxo@users.noreply.github.com>
2023-09-29 12:55:07 +0200

    Initial commit

---
+++ src/main/java/BenZamey/FilmZee/FilmZeeApplication.java
@@ -0,0 +1,14 @@
+package BenZamey.FilmZee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmZeeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FilmZeeApplication.class, args);
+	}
+	//1h 15m:52s
+}
---
+++ src/test/java/BenZamey/FilmZee/FilmZeeApplicationTests.java
@@ -0,0 +1,13 @@
+package BenZamey.FilmZee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmZeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
